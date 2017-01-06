6b1030c30796bb00576e8d9cf9a7607f6a7c5d1e
Olaking1 <1121725720@qq.com>
2017-01-06 21:20:49 +0800

    8

--- LDAFortopic.py
+++ LDAFortopic.py
@@ -4,8 +4,8 @@ from gensim import corpora, models, similarities
 from scipy.sparse import csr_matrix
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import numpy as np
+# import regex as re
 import string
-import regex as re
 import os, re, time, logging
 import jieba
 import pickle as pkl
@@ -16,7 +16,7 @@ import pickle as pkl
 #                     datefmt='%a, %d %b %Y %H:%M:%S',
 #                     )
 
-class loadFolders(object):  #迭代器
+class loadFolders(object):  # 迭代器
     def __init__(self, par_path):
         self.par_path = par_path
 
@@ -38,71 +38,62 @@ class loadFiles(object):
             for file in os.listdir(folder):  # secondary directory
                 file_path = os.path.join(folder, file)
                 if os.path.isfile(file_path):
-                    this_file = open(file_path, 'r')
+                    this_file = open(file_path, 'r', encoding='utf-8')
                     try:
                         content = this_file.read()
-                        str = ''
-                        for i in range(len(string.punctuation)):
-                            str += ' '
-                        table = content.maketrans(string.punctuation, str)
-                        content = content.translate(table)
-                        # content = ' '.join(filter(lambda x: x, content.split(' ')))
                     except UnicodeDecodeError:
-                        # print(file_path)
-                        content = ""
+                        print(file_path)
+                        content = ''
                         # this_file = open(file_path, 'rb')
                         # content = this_file.read()
                         this_file.close()
                     yield catg, file, content
                     this_file.close()
 
-    def delete_punctuation(self, content):
-        str = ''
-        for i in range(len(string.punctuation)):
-            str += ' '
-        table = content.maketrans(string.punctuation, str)
-        content = content.translate(table)
-        return content
-
 
 def convert_doc_to_wordlist(str_doc, cut_all):
     sent_list = str(str_doc).split('\n')
-    sent_list = map(rm_char, sent_list)  # 去掉一些字符，例如中文空格
-    jieba.load_userdict('/Users/liyue/Documents/datasets/use_words')
+    # sent_list = map(rm_char, sent_list)  # 去掉一些字符，例如中文空格
+    jieba.load_userdict('../../datasets/use_words')
     word_2dlist = [rm_tokens(jieba.cut(re.sub("\p{P}+", "", part.lower()), cut_all=cut_all)) for part in
-                   sent_list]  # 分词
+                   sent_list]  #分词
     word_list = sum(word_2dlist, [])
     return word_list
-
 
 def rm_tokens(words):  # 去掉一些停用词和数字
     words_list = list(words)
     stop_words = get_stop_words()
+    str = ''
+    for i in range(len(string.punctuation)):
+        str += ' '
+
     for i in range(words_list.__len__())[::-1]:
+        table = words_list[i].maketrans(string.punctuation, str)
+        words_list[i] = words_list[i].translate(table)
         if words_list[i] in stop_words:  # 去除停用词
             words_list.pop(i)
         elif words_list[i] in gensim.parsing.preprocessing.STOPWORDS:  # 去掉停用词
             words_list.pop(i)
         elif words_list[i].isdigit():
             words_list.pop(i)
-        elif words_list[i].isspace():
-            words_list.pop(i)
+        # elif words_list[i].isspace():
+        #     words_list.pop(i)
+        # words_list[i] = gensim.parsing.preprocessing.strip_punctuation(words_list[i])
     return words_list
 
-def get_stop_words(path='/Users/liyue/Documents/datasets/stop_words'):
-    file = open(path, 'r')
+
+def get_stop_words(path='../../datasets/stop_words'):
+    file = open(path, 'r', encoding='utf-8')
     file = file.read().split('\n')
     return set(file)
 
-
-def rm_char(text):
-    text = re.sub(' ', '', text)
-    return text
-
+# def rm_char(text):
+#     text = re.sub(' ', '', text)
+#     return text
 
 if __name__ == '__main__':
-    path_doc_root = '/Users/liyue/Documents/datasets/csdn_after'  # 根目录 即存放按类分类好的文本集
-    path_tmp = '/Users/liyue/Documents/datasets/csdn_tmp'  # 存放中间结果的位置
+    path_doc_root = '../../datasets/csdn_after'  # 根目录 即存放按类分类好的文本集
+    path_tmp = '../../datasets/csdn_tmp'  # 存放中间结果的位置
     path_dictionary = os.path.join(path_tmp, 'THUNews.dict')
     path_tmp_corpus = os.path.join(path_tmp, 'corpus')
     path_tmp_tfidf = os.path.join(path_tmp, 'tfidf_corpus')
@@ -110,6 +101,7 @@ if __name__ == '__main__':
     path_tmp_ldamodel = os.path.join(path_tmp, 'lda_model.pkl')
     path_tmp_predictor = os.path.join(path_tmp, 'predictor.pkl')
     n = 1  # n 表示抽样率， n抽1
+    n_topic = 9
 
     dictionary = None
     corpus = []
@@ -141,7 +133,7 @@ if __name__ == '__main__':
                           .format(i=i, t=time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())))
         print("去掉出现次数过多或过少的词前，字典长度为：" + str(len(dictionary)))
         # 去掉词典中出现次数过少或过多的
-        small_freq_ids = [tokenid for tokenid, docfreq in dictionary.dfs.items() if docfreq < 5]
+        small_freq_ids = [tokenid for tokenid, docfreq in dictionary.dfs.items() if docfreq < 20]
         dictionary.filter_tokens(small_freq_ids)
         dictionary.compactify()
         dictionary.save(path_dictionary)
@@ -191,7 +183,7 @@ if __name__ == '__main__':
     else:
         print('=== 检测到tfidf向量已经生成，跳过该阶段 ===')
 
-    # # ====================================================================
+    # # ===================================================================
     # # # # 第三阶段，  开始将tfidf转化成lda
     if not os.path.exists(path_tmp_lda):
         print('=== 未检测到有lda文件夹存在，开始生成lda向量 ===')
@@ -222,7 +214,7 @@ if __name__ == '__main__':
         for catg in catgs:
             tmp = corpus_tfidf.get(catg)
             corpus_tfidf_total += tmp
-        lda_model = models.LdaModel(corpus=corpus_tfidf_total, id2word=dictionary, num_topics=7, alpha='auto',
+        lda_model = models.LdaModel(corpus=corpus_tfidf_total, id2word=dictionary, num_topics=n_topic, alpha='auto',
                                     eval_every=1)
         # 将lda模型存储到磁盘上
         lda_file = open(path_tmp_ldamodel, 'wb')
@@ -279,27 +271,32 @@ if __name__ == '__main__':
                 filenames.append(filename)
                 doc.append(msg[2])
 
-    # gamma = lda_model.do_estep(corpus, state=lda_model.state)
-    # lda_model.update_alpha(gamma, 0.7)
-    # ldaState = models.ldamodel.LdaState(eta=0.7, shape=(lda_model.num_topics, lda_model.num_terms))
-    # lda_model.optimize_eta = True
-    # lda_model.do_mstep(rho=0.3, other=ldaState)
-    topic = lda_model.print_topics(7, 15)
+    gamma = lda_model.do_estep(corpus, state=lda_model.state)
+    lda_model.update_alpha(gamma, 0.7)
+    ldaState = models.ldamodel.LdaState(eta=0.7, shape=(lda_model.num_topics, lda_model.num_terms))
+    lda_model.optimize_eta = True
+    lda_model.do_mstep(rho=0.3, other=ldaState)
+    topic = lda_model.show_topics(n_topic, 5)
     print(topic)
-    index = similarities.docsim.Similarity(output_prefix='/Users/liyue/Documents/datasets/csdn_tmp', corpus=corpus,
-                                           num_features=len(dictionary))
-    filePath = os.path.join(path_tmp,'similaritiesFiles')
-    file = open(filePath, 'w')#若文件存在，则先清空再打开
-    for i, similarities in zip(range(len(dictionary)), index):
-        file.writelines("======================================")
-        file.write('\r\n')
-        file.writelines("%d 与 %s相似的文档为："%(i, filenames[i]))
-        file.write('\r\n')
-        boolSimi = similarities > 0.7
-        for j in range(len(boolSimi)):
-            if(boolSimi[j]):
-                file.writelines(str(similarities[j])+":"+filenames[j])
-                file.write('\r\n')
-    file.close()
+    # index = similarities.docsim.Similarity(output_prefix=path_tmp, corpus=corpus,
+    #                                        num_features=len(dictionary))
+    # filePath = os.path.join(path_tmp,'similaritiesFiles')
+    # file = open(filePath, 'a', encoding='utf-8')
+    # for i, similarities in zip(range(len(dictionary)), index):
+    #     file.writelines("================================================")
+    #     file.write('\r\n')
+    #     file.writelines("【【【%d 与 %s相似的文档为：】】】"%(i, filenames[i]))
+    #     file.write('\r\n')
+    #     boolSimi = similarities > 0.85
+    #     for j in range(len(boolSimi)):
+    #         if(boolSimi[j]):
+    #             file.writelines(str(similarities[j])+":"+filenames[j])
+    #             file.write('\r\n')
+    # file.close()
+
+    documentTopic = lda_model.get_document_topics(corpus[2],minimum_phi_value=0.02)
+    topicList = lda_model.get_topic_terms(1,10)
+    for i in range(len(documentTopic)):
+        print(dictionary[documentTopic[i][0]])
 
     print("Log perplexity of the model is", lda_model.log_perplexity(corpus))
